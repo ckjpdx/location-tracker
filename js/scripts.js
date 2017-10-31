12a8ecebf5f40d9daee0dd2e83baf320c3c5f4ca
Chris Johnson <cknightjohnson@gmail.com>
2017-10-30 21:53:13 -0700

    adds partial object referencing system

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-function Visit(country, location, landmarks, time, notes) {
+function Visit(visitId, country, location, landmarks, time, notes) {
+  this.visitId = visitId;
   this.country = country;
   this.location = location;
   this.landmarks = landmarks;
@@ -8,6 +9,7 @@ function Visit(country, location, landmarks, time, notes) {
 
 $(document).ready(function() {
   var visits = [];
+  var visitId = 0;
   $("#visited-form").submit(function(event) {
     event.preventDefault();
     var country = $("input#input-country").val();
@@ -15,20 +17,34 @@ $(document).ready(function() {
     var landmarks = $("input#input-landmarks").val();
     var time = $("input#input-time").val();
     var notes = $("input#input-notes").val();
-    var newVisit = new Visit(country, location, landmarks, time, notes);
+    var newVisit = new Visit(visitId, country, location, landmarks, time, notes);
     visits.push(newVisit);
-
-    $("ul#visitedPlaces").append("<li><span class='registeredLocation visits-index" + (visits.length - 1) + "'>" + newVisit.country + "</span></li>");
+    $("ul#visitedPlaces").append("<li><span class='registeredLocation' id='" + visitId + "'>" + newVisit.country + "</span></li>");
+    visitId++;
     $("input").each(function(){
       $(this).val("");
     });
     $('.registeredLocation').last().click(function(){
-      $(".place-info").show();
-      $(".place-country").text(newVisit.country);
-      $(".place-location").text(newVisit.location);
-      $(".place-landmarks").text(newVisit.landmarks);
-      $(".place-time").text(newVisit.time);
-      $(".place-notes").text(newVisit.notes);
+      var thisId = $(this).attr('id');
+      var thisObj = {};
+      visits.forEach(function(each){
+        if (thisId === each.visitId) {
+          thisObj = each;
+          console.log(each);
+          console.log(thisObj);
+          $(".place-country").text(thisObj.country);
+          $(".place-location" + thisId).text(thisObj.location);
+          $(".place-landmarks" + thisId).text(thisObj.landmarks);
+          $(".place-time" + thisId).text(thisObj.time);
+          $(".place-notes" + thisId).text(thisObj.notes);
+        } else {
+          thisObj = each;
+          console.log('else');
+          console.log(thisId);
+          console.log(each);
+          console.log(thisObj);
+        }
+      });
     });
   });
 });
